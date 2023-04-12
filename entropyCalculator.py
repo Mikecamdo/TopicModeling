79bd0dd10db67351f17c24f420bf1ebd0c836426
Mikecamdo <mcdoherty@smu.edu>
2023-04-11 23:56:27 -0500

    Calculating Entropies
    Calculating the entropies of the real and LDA generated topics

--- entropyCalculator.py
+++ entropyCalculator.py
@@ -101,15 +101,73 @@ sorted_temp = {i: temp[i] for i in theKeys}
 origTopics = ['NFL', 'XFL', 'NBA', 'Facebook', 'Twitter', 'France', 'Mexico', 'World War II', 'Cybersecurity', 'Marvel']
 counter = 0
 topic_counter = 0
-entropy_finder = { }
+entropy_finder_articles = [ ]
+entropy_finder_topics = [ ]
 for key in sorted_temp:
-    print(origTopics[topic_counter], (counter + 1),':', sorted_temp[key])
-    entropy_finder.update({origTopics[topic_counter] + ' ' + str((counter + 1)) + ':' : sorted_temp[key]})
+    entropy_finder_articles.append(origTopics[topic_counter] + ' ' + str((counter + 1)) + ':')
+    entropy_finder_topics.append(sorted_temp[key])
     counter += 1
     if counter == 20:
         topic_counter += 1
         counter = 0
 
 import numpy as np
+for i in range(200):
+    print(entropy_finder_articles[i], entropy_finder_topics[i])
 
 origTopicsNums = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20]
+
+#Real Topics
+nfl_entropy = -(18/20) * np.log2(18/20) - (1/20) * np.log2(1/20) - (1/20) * np.log2(1/20)
+
+xfl_entropy = -(15/20) * np.log2(15/20) - (4/20) * np.log2(4/20) - (1/20) * np.log2(1/20)
+
+nba_entropy = -(2/20) * np.log2(2/20) - (3/20) * np.log2(3/20) - (15/20) * np.log2(15/20)
+
+facebook_entropy = -(1/20) * np.log2(1/20) - (1/20) * np.log2(1/20) - (18/20) * np.log2(18/20)
+
+twitter_entropy = -(1/20) * np.log2(1/20) - (2/20) * np.log2(2/20) - (15/20) * np.log2(15/20) - (2/20) * np.log2(2/20)
+
+france_entropy = -(2/20) * np.log2(2/20) - (1/20) * np.log2(1/20) - (13/20) * np.log2(13/20) - (1/20) * np.log2(1/20) - (3/20) * np.log2(3/20)
+
+mexico_entropy = -(14/20) * np.log2(14/20) - (1/20) * np.log2(1/20) - (2/20) * np.log2(2/20) - (2/20) * np.log2(2/20) - (1/20) * np.log2(1/20)
+
+ww2_entropy = -(12/20) * np.log2(12/20) - (1/20) * np.log2(1/20) - (2/20) * np.log2(2/20) - (2/20) * np.log2(2/20) - (3/20) * np.log2(3/20)
+
+cybersecurity_entropy = -(2/20) * np.log2(2/20) - (1/20) * np.log2(1/20) - (5/20) * np.log2(5/20) - (12/20) * np.log2(12/20)
+
+marvel_entropy = -(17/20) * np.log2(17/20) - (1/20) * np.log2(1/20) - (2/20) * np.log2(2/20)
+
+#LDA Topics
+topic1_entropy = -(18/35) * np.log2(18/35) - (15/35) * np.log2(15/35) - (2/35) * np.log2(2/35)
+topic2_entropy = -(1/32) * np.log2(1/32) - (1/32) * np.log2(1/32) - (2/32) * np.log2(2/32) - (14/32) * np.log2(14/32) - (12/32) * np.log2(12/32) - (2/32) * np.log2(2/32)
+topic3_entropy = -(1/8) * np.log2(1/8) - (4/8) * np.log2(4/8) - (3/8) * np.log2(3/8)
+topic4_entropy = -(15/17) * np.log2(15/17) - (1/17) * np.log2(1/17) - (1/17) * np.log2(1/17)
+topic5_entropy = -(1/18) * np.log2(1/18) - (1/18) * np.log2(1/18) - (13/18) * np.log2(13/18) - (1/18) * np.log2(1/18) - (2/18) * np.log2(2/18)
+topic6_entropy = -(18/26) * np.log2(18/26) - (2/26) * np.log2(2/26) - (1/26) * np.log2(1/26) - (2/26) * np.log2(2/26) - (2/26) * np.log2(2/26) - (1/26) * np.log2(1/26)
+topic7_entropy = -(1/18) * np.log2(1/18) - (17/18) * np.log2(17/18)
+topic8_entropy = -(15/29) * np.log2(15/29) - (3/29) * np.log2(3/29) - (2/29) * np.log2(2/29) - (3/29) * np.log2(3/29) - (5/29) * np.log2(5/29) - (1/29) * np.log2(1/29)
+topic9_entropy = -(1/13) * np.log2(1/13) - (12/13) * np.log2(12/13)
+topic10_entropy = -(2/4) * np.log2(2/4) - (2/4) * np.log2(2/4)
+
+print('NFL', nfl_entropy)
+print('XFL', xfl_entropy)
+print('NBA', nba_entropy)
+print('Facebook', facebook_entropy)
+print('Twitter', twitter_entropy)
+print('France', france_entropy)
+print('Mexico', mexico_entropy)
+print('World War 2', ww2_entropy)
+print('Cybersecurity', cybersecurity_entropy)
+print('Marvel', marvel_entropy)
+
+print('LDA Topic 1', topic1_entropy)
+print('LDA Topic 2', topic2_entropy)
+print('LDA Topic 3', topic3_entropy)
+print('LDA Topic 4', topic4_entropy)
+print('LDA Topic 5', topic5_entropy)
+print('LDA Topic 6', topic6_entropy)
+print('LDA Topic 7', topic7_entropy)
+print('LDA Topic 8', topic8_entropy)
+print('LDA Topic 9', topic9_entropy)
+print('LDA Topic 10', topic10_entropy)
